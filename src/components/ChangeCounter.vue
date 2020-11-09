65b46b9fee9ef4bc98f75447f164e104c1b91750
Eyoatam <eyoatamtamirat7@gmail.com>
2020-11-09 16:11:10 +0300

    initial commit

---
+++ src/components/ChangeCounter.vue
@@ -0,0 +1,20 @@
+<template>
+	<button @click="addTwo">increment</button>
+	<button @click="addEleven({ value: 10 })">increase</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+	methods: {
+		// addOne() {
+		// 	this.$store.dispatch('increment');
+		// },
+		// ...mapActions(['increment', 'increase']),
+		...mapActions('numbers', {
+			addTwo: 'increment',
+			addEleven: 'increase',
+		}),
+	},
+};
+</script>
---
+++ src/components/UserAuth.vue
@@ -0,0 +1,20 @@
+<template>
+	<button @click="login" v-if="!isAuth">Login</button>
+	<button @click="logout" v-if="isAuth">Logout</button>
+	<p>{{ isAuth }}</p>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import { mapActions } from 'vuex';
+export default {
+	methods: {
+		...mapActions(['login', 'logout']),
+	},
+	computed: {
+		...mapGetters({
+			isAuth: 'userIsAuth',
+		}),
+	},
+};
+</script>
